47db38690f890d17c1920a8e040efc831d96be2a
Dmitriy Zaytsev <dmitriy_zaitsev@mail.ru>
2023-02-03 20:46:52 +0300

    Исправил замечания https://startjava.slack.com/archives/C04J97N2KEW/p1675438053729409

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -15,22 +15,22 @@ public class CyclesTheme {
     static void calculateSumOfEvenAndOddNumbers() {
         System.out.println("1. Подсчет суммы четных и нечетных чисел");
 
-        byte num = -10;
-        int evenSum = 0;
-        int oddSum = 0;
+        byte counter = -10;
+        int sumOfEvenNum = 0;
+        int sumOfOddNum = 0;
 
         do {
-            if (num % 2 == 0) {
-                evenSum += num++;
+            if (counter % 2 == 0) {
+                sumOfEvenNum += counter;
             } else {
-                oddSum += num++;
+                sumOfOddNum += counter;
             }
-
-        } while (num <= 21);
+            counter++;
+        } while (counter <= 21);
 
         System.out.printf("In the interval [-10, 21] the sum of the even numbers is %d and the " +
-                "sum of odd numbers is %d\n\n",
-                evenSum, oddSum);
+                        "sum of odd numbers is %d\n\n",
+                sumOfEvenNum, sumOfOddNum);
     }
 
     static void showNumbersInDescOrder() {
@@ -40,50 +40,47 @@ public class CyclesTheme {
         int num2 = 5;
         int num3 = -1;
 
-        int minNum = 10;
-        int avgNum = num2;
-        int maxNum = num3;
+        int minNum = num1;
+        int maxNum = num1;
 
-        for (int i = 1; i < 3; i++) {
-            if (minNum > avgNum) {
-                minNum ^= avgNum;
-                avgNum ^= minNum;
-                minNum ^= avgNum;
-            }
-
-            if (avgNum > maxNum) {
-                maxNum ^= avgNum;
-                avgNum ^= maxNum;
-                maxNum ^= avgNum;
-            }
+        if (num2<minNum) {
+          minNum = num2;
+        }
+        if(num3<minNum){
+            minNum = num3;
+        }
+        if(num2 > maxNum){
+            maxNum = num2;
+        }
+        if(num3 > maxNum){
+            maxNum = num3;
         }
 
-        System.out.printf("Numbers in descending order: %d, %d, %d\n\n", maxNum, avgNum, minNum);
+        System.out.printf("the original numbers %d %d %d \n", num1, num2, num3);
+
+        for (int i = minNum + 1; i < maxNum; i++) {
+            System.out.printf("%d ", i);
+        }
     }
 
     static void showFiguresInRevOrderWithSum() {
-        System.out.println("3. Вывод реверсивного числа и суммы его цифр");
+        System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
 
         int srcNum = 1234;
-        int reverseNum = 0;
-        int sumOfFigures = 0;
+        int sumOfDigits = 0;
+        int remainder = srcNum;
+        int digit = 0;
 
-        int base = 1;
-        int reverseBase = 1000;
-        int figure = 0;
+        System.out.printf("an original number %d\nthe reversed number ", srcNum);
 
-        while (base <= 1000) {
-            figure = srcNum / base % 10;
-            reverseNum += figure * reverseBase;
-            sumOfFigures += figure;
-
-            base *= 10;
-            reverseBase /= 10;
+        while (remainder >= 1) {
+            digit = remainder % 10;
+            System.out.printf("%d", digit);
+            sumOfDigits += digit;
+            remainder /= 10;
         }
 
-        System.out.printf("an original number %d, the number in reverse order of its figures %d, " +
-                "the sum of its figures %d\n\n",
-                srcNum, reverseNum, sumOfFigures);
+        System.out.printf("\nthe sum of its figures %d\n\n", sumOfDigits);
     }
 
     static void printNumbersInMultLines() {
@@ -91,16 +88,18 @@ public class CyclesTheme {
 
         int maxNum = 24;
         int numbersInLine = 5;
-        int num;
+        int counter = 1;
 
-        for (int i = 0; i < maxNum / numbersInLine + 1; i++) {
-            num = i * numbersInLine;
-            System.out.printf("%2d %2d %2d %2d %2d\n",
-                    num,
-                    ++num >= maxNum ? 0 : num,
-                    ++num >= maxNum ? 0 : num,
-                    ++num >= maxNum ? 0 : num,
-                    ++num >= maxNum ? 0 : num);
+        for (int i = 0; i < maxNum / (2*numbersInLine) + 1; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(counter >= maxNum){
+                    System.out.printf("%2d ", 0);
+                } else {
+                    System.out.printf("%2d ", counter);
+                }
+                counter += 2;
+            }
+            System.out.println();
         }
     }
 
@@ -242,7 +241,7 @@ public class CyclesTheme {
 
         if (sumLeft == sumRight) {
             System.out.printf("A lucky number, sum of the left figures is equal to the right ones" +
-                    " %s = %s\n",
+                            " %s = %s\n",
                     left, right);
         } else {
             System.out.printf("Not a lucky number, sum of the left figures is not equal to the " +
