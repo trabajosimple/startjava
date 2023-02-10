d9c8eeaa7752a469719974acdf0283956cc32e27
Dmitriy Zaytsev <dmitriy_zaitsev@mail.ru>
2023-02-10 17:17:41 +0300

    Выполнил первое задание второго урока

---
+++ Lesson_2/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    public char gender;
+    public String name;
+    public int height;
+    public int weight;
+    public int age;
+
+    public Person() {
+        this.gender = 'M';
+        this.name = "Moisey Petrovich Grigorian";
+        this.height = 153;
+        this.weight = 120;
+        this.age = 89;
+    }
+
+    public void walk() {
+    }
+
+    public void sit() {
+    }
+
+    public void run() {
+    }
+
+    public void talk() {
+    }
+
+    public void learnJava() {
+    }
+}
